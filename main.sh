a25213f91dc4154be1b4222892507bce3fe3e687
fluffy <thefluffysnowmanyt@gmail.com>
2022-04-10 16:33:58 +0530

    more changes

--- main.sh
+++ main.sh
@@ -45,16 +45,30 @@ do
 			nmap -sn $ipsub_scanvar_0x1
 
 		fi
+	elif [[ $prompt_input == "fapi" ]] ; then
+		read -p "API url with auth key: " api_url1
+		echo "ENTERED API URL: $api_url1"
 
+		read -p "Display api raw data? (y/n): " rawdata_ask
+		if [[ $rawdata_ask == "n" ]] ; then
+			echo "Not displaying raw data"
+			read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
+			curl -s $api_url1 | jq ".$array_header_0x1"
+		else
+			curl -s $api_url1 | jq "."
+		fi
+	elif [[ $prompt_input == "ping" ]] ; then
+		read -p "Enter host to ping>>> " ping_host_0x1
+		read -p "How many seconds to ping>>> " ping_seconds_0x1
+		sudo ping -c $ping_seconds_0x1 $ping_host_0x1
 	elif [[ $prompt_input == "help" ]] ; then
+
+		echo "help: displays this help menu"
 		echo "netmap: opens network mapping module"
 		echo "ping: opens ping module"
 		echo "fapi: opens fing api calling module"
 		echo "exit: exits program"
-	#read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
-	#curl -s $api_url1 | jq ".$array_header_0x1"
 
-	
 	fi
 
 	let "increment=increment+1"
