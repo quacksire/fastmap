0bfb2083c9d093d9a540bb677c4f7f811c6f971f
fluffy <thefluffysnowmanyt@gmail.com>
2022-04-10 13:10:15 +0530

    more updates on network auditing

--- fingtestapi1.sh
+++ fingtestapi1.sh
@@ -6,8 +6,34 @@ read -p "Press [ENTER] to start program"
 
 echo ""
 
-figlet -f slant "Netowrk API\
- Calling Tool" | lolcat
+figlet -f slant "Netowrk Auditing\
+ & Pinging Tool" | lolcat
+
+read -p "Value of i: " increment
+echo ""
+echo "NOW THE PROGRAM WILL LOOP. PRESS CNTL+C TO QUIT"
+echo ""
+
+while [ $increment -lt 100 ]
+do
+
+	read -p ">>>  " prompt_input
+	if [ prompt_input == "exit" ]
+	then
+		break
+	fi 
+	if [ prompt_input == "help" ]
+	then
+		echo "netmap: opens network mapping module"
+		echo "ping: opens ping module"
+
+		echo "exit: exits program"
+	#read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
+	#curl -s $api_url1 | jq ".$array_header_0x1"
+
+	let "increment=increment+1"
+
+done
 
 read -p "API url with auth key: " api_url1
 echo "ENTERED API URL: $api_url1"
@@ -19,23 +45,5 @@ then
 else
 	curl -s $api_url1 | jq "."
 fi
-
-read -p "Value of i: " increment
-echo ""
-echo "NOW THE PROGRAM WILL LOOP. PRESS CNTL+C TO QUIT"
-echo ""
-
-while [ $increment -lt 100 ]
-do
-
-	read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
-	curl -s $api_url1 | jq ".$array_header_0x1"
-
-	let "increment=increment+1"
-
-done
-
-
-
 
 
