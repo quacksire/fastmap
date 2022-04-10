6662a9af711554efa4637f64b8bbdc3373e9e220
fluffy <thefluffysnowmanyt@gmail.com>
2022-04-10 14:54:25 +0530

    nmap some started

--- main.sh
+++ main.sh
@@ -23,19 +23,42 @@ do
 	if [[ $prompt_input == "exit" ]] ; then
 		echo "Exiting program"
 		break
+	elif [[ $prompt_input == "netmap" ]] ; then
+
+		echo "Starting nmap module"
+		read -p "Enter host+/subnet to scan>>> " ipsub_scanvar_0x1
+		read -p "What type of scan do you want to do? (syn/port(tcp)/os/arp)>>> " scan_type_0x1
+
+		if [[ $scan_type_0x1 == "port(tcp)" ]] ; then
+
+			reap -p "Scan all TCP ports(y/n)>>> " scanyesnotcp
+
+			if [[ $scanyesnotcp == "y" ]] ; then
+				nmap -p0- -A -T4 -v $ipsub_scanvar_0x1
+			else
+				echo "scan cancelled"
+			fi
+		
+		elif [[ $scan_type_0x1 == "arp" ]] ; then
+
+			echo "Commencing arp scan of subnet"
+			nmap -sn $ipsub_scanvar_0x1
+
+		fi
+
 	elif [[ $prompt_input == "help" ]] ; then
 		echo "netmap: opens network mapping module"
 		echo "ping: opens ping module"
-
+		echo "fapi: opens fing api calling module"
 		echo "exit: exits program"
 	#read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
 	#curl -s $api_url1 | jq ".$array_header_0x1"
 
-		
+	
 	fi
 
 	let "increment=increment+1"
-	
+
 done
 
 #read -p "API url with auth key: " api_url1
