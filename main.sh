fbb2781bea17167aed4222c12d9e271c942753f1
fluffy <thefluffysnowmanyt@gmail.com>
2022-04-10 14:14:14 +0530

    fixing bugs

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "PREREQUISITES: figlet, curl, jq, lolcat"
 echo "PLEASE INSTALL THESE BINARIES WITH YOUR PACKAGE MANAGER"
 echo "NOTE: USE LOWER CASE FOR ALL INPUTS"
@@ -18,12 +20,10 @@ while [ $increment -lt 100 ]
 do
 
 	read -p ">>>  " prompt_input
-	if [ prompt_input == "exit" ]
-	then
+	if [[ $prompt_input == "exit" ]] ; then
+		echo "Exiting program"
 		break
-	fi 
-	if [ prompt_input == "help" ]
-	then
+	elif [[ $prompt_input == "help" ]] ; then
 		echo "netmap: opens network mapping module"
 		echo "ping: opens ping module"
 
@@ -31,19 +31,22 @@ do
 	#read -p "Enter array values to query (with dot notation for jq[except for the first delimiter]): " array_header_0x1
 	#curl -s $api_url1 | jq ".$array_header_0x1"
 
-	let "increment=increment+1"
+		
+	fi
 
+	let "increment=increment+1"
+	
 done
 
-read -p "API url with auth key: " api_url1
-echo "ENTERED API URL: $api_url1"
+#read -p "API url with auth key: " api_url1
+#echo "ENTERED API URL: $api_url1"
 
-read -p "Display api raw data? (y/n): " rawdata_ask
-if [ $rawdata_ask == "n" ]
-then
-	echo "Not displaying raw data"
-else
-	curl -s $api_url1 | jq "."
-fi
+#ead -p "Display api raw data? (y/n): " rawdata_ask
+#if [ $rawdata_ask == "n" ]
+#then
+#	echo "Not displaying raw data"
+#else
+#	curl -s $api_url1 | jq "."
+#fi
 
 
