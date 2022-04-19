5349c6ba5915fff83163022558874c5afca15073
fluuffy <thefluffysnowmanyt@gmail.com>
2022-04-19 11:46:13 +0530

    fixed :)

--- main.sh
+++ main.sh
@@ -164,7 +164,8 @@ do
 		sudo python3 pymap.py -t $pymap_0x1 > online_hosts_pymap.txt
 		echo "Calculating total amount of online hosts..."
 		cat online_hosts_pymap.txt
-		cat online_hosts_pymap.txt | grep -v -e "-----------------------------------" -e "IP Address" -e "MAC Address" | wc -l
+		cat online_hosts_pymap.txt | grep -v -e "-----------------------------------" -e "IP Address" -e "MAC Address" | wc -l > total_hosts_online_pymap.txt
+		cat total_hosts_online_pymap.txt && echo " hosts online"
 
 		echo "================================================================" | lolcat
 	
